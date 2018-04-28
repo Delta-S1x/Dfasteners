9cdd4cade0907e2753f3feec3f595dcf9a69ea57
Delta <dustin.williams92@yahoo.com>
2018-04-28 00:46:33 -0400

    Added some more equations in, made a function to determine length of roof panels. Added RoofClips to the Collection dict

--- Dfasteners.py
+++ Dfasteners.py
@@ -1,8 +1,11 @@
 
-import xlrd,sys,re
+import xlrd,sys,re,math
 
 
 parts = {}
+fastners = {}
+roofpanelsnumber = []
+roofpanelslength = []
 ########################################
 
 from xlrd import open_workbook
@@ -15,7 +18,8 @@ def main_questionair():
     print('Note: All firewalls will be treated as 3 hour firewalls. So adjust gypboard fasteners and compound accordingly ')
    # try:
     global b_loc
-    path = "/root/1.xls"#input("PLEASE COPY AND PAST THE FILE PATH TO THE TAKEOFF\n")
+    global bldg
+    path = "C:\Downloads\\1.xls"#input("PLEASE COPY AND PAST THE FILE PATH TO THE TAKEOFF\n")
     bldg = "bldg 1"#input('WHAT IS THE NAME OF THE BUILDING ON THE SPREADSHEET?\n').lower()
 
     workbook = open_workbook(path)
@@ -71,7 +75,7 @@ def PartsSearch():
                   ["16WallColumn",["CEBA"]],["18WallColumn",["CEBB"]],["CornerColumn",["CEAA"]],
                   ["24CornerColumn",["CEBC"]],["InsideCorner",["CEAF"]],["22gaColumn",["CIAD"]],
                   ["7'ScrewGuard", ["S50180"]],["7'10ScrewGuard",["S50049Gl"]],["JambChanngel",["S46185"]],
-                  ]
+                  ["20'gutter",["T30218"]],["20'3gutter",["T30219"]],["RidgeCap",["R30025GL"]], ["RoofClips",["R20059"]]]
     for arguments in collection:
         key, string = arguments
         excell_pull_routine(key,string)
@@ -86,7 +90,7 @@ def PartsSearch():
         #print (value)
         if "SBA" in value:
             num = re.findall(r'\d{3}', value)
-            num = int(num[0])
+            num = num[0]
             if num:
                 if num in range(80):
                     parts["5'nominal_base"] += sheet.cell(r, b_loc).value
@@ -96,14 +100,105 @@ def PartsSearch():
                     parts["15'nominal_base"] += sheet.cell(r, b_loc).value
 
 
+    for r in range(sheet.nrows):
+
+        cell = sheet.cell(r,0)
+        value = cell.value
+        if "B4" in value or "BDS" in value:
+            num = re.findall(r'\d{3}', value)
+            num = num[0]
+            roofpanelslength.append(float(num)/12)
+            roofpanelsnumber.append((sheet.cell(r, b_loc).value))
+
+
+
+
+
+
+
+
+
+
+#########################################################################################
+def Add_Fastner(key,value):
+
+    fastners[key] += value
+
+def Fastner_Calcs():
+    global SidewallLength
+    if parts["RidgeCap"] == 0:
+        SidewallLength = (int(parts["316Panels"]) * 16 / 12)
+    else:
+        SidewallLength = input(("What is the total length of (Just give the number in feet)", bldg,"\n"))
+
+
+    #M20050
+    fastners["M20050"] = 0
+    M20050 = [((parts['downspouts'] + parts['overflows']) / 10),
+            ((parts["7'10ScrewGuard"] + parts["7'ScrewGuard"]) / 8)]
+
+
+    if parts["20'gutter"] == 0:
+        M20050.append((SidewallLength) * 2/40)
+
+    for x in M20050:
+        Add_Fastner("M20050",x)
+
+
+    #########
+    #M20055
+    fastners["M20055"] = 0
+    M20055 = [(SidewallLength * 2/ 40),
+             ((parts["7'10ScrewGuard"] + parts["7'ScrewGuard"]) / 8 * 0.2),
+              (SidewallLength * 2 / 30),
+             (parts["RoofPanelClosures"] * 10 / 30)]
+    if parts["20'gutter"] != 0:
+        M20055.append(SidewallLength * 2/40)
+    for x in M20055:
+        Add_Fastner("M20055",x)
+
+    #M30001
+    fastners["M30001"] = 0
+    PanelsxLength = 0
+    print(roofpanelslength)
+    for x in range(len(roofpanelslength)):
+        PanelsxLength += roofpanelslength[x] * roofpanelsnumber[x]
+    M30001 = [PanelsxLength,
+              (parts["RoofClips"] / 80),
+              (parts["GutterClips"] / 150)]
+    for x in M30001:
+        Add_Fastner("M30001",x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    for key in list(fastners):
+        fastners[key] = math.ceil(fastners[key])
+
 
 
 
 
 main_questionair()
-#def Fastner_calcs():
-M20050 = [((parts['downspouts'] + parts['overflows']) / 10) +
-        ((parts["7'10ScrewGuard"] + parts["7'ScrewGuard"]) / 8)
-]
+Fastner_Calcs()
 
-print (M20050)
+
+
+print(fastners)
+
+
+
+
+
+##########################NOTES################################
+#FIGURE OUT 1 PER 40FT OF FLASHING/COUNTER FLASHING AT MASONRY M20055
